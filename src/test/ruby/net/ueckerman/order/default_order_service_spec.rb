f322f475447bff76f6e020d331bc554251750113
dueckes <matthew@ueckerman.net>
2011-10-06 00:07:55 +0200

    let preferred over instance variables initialised in before blocks

--- src/test/ruby/net/ueckerman/order/default_order_service_spec.rb
+++ src/test/ruby/net/ueckerman/order/default_order_service_spec.rb
@@ -13,61 +13,58 @@ import Java::net.ueckerman.user.UserMother
 
 describe DefaultOrderService do
 
-  before(:each) do
-    @products = ProductMother.createProducts
-    @user = UserMother.createUser
+  let(:products) { ProductMother.createProducts }
+  let(:user) { UserMother.createUser }
+  let(:unsaved_order) { OrderMother.createOrder }
+  let(:saved_order) { OrderMother.createOrder }
 
-    @unsaved_order = OrderMother.createOrder
-    @saved_order = OrderMother.createOrder
+  let(:order_factory) { double(OrderFactory, :create => unsaved_order) }
+  let(:order_repository) { double(OrderRepository, :save => saved_order) }
+  let(:payment_gateway) { double(PaymentGateway, :payFor => nil) }
 
-    @order_factory = double(OrderFactory, :create => @unsaved_order)
-    @order_repository = double(OrderRepository, :save => @saved_order)
-    @payment_gateway = double(PaymentGateway, :payFor => nil)
-
-    @default_order_service = DefaultOrderService.new(@order_factory, @order_repository, @payment_gateway)
-  end
+  let(:default_order_service) { DefaultOrderService.new(order_factory, order_repository, payment_gateway) }
 
   context "#placeOrderFor" do
 
     it "should persist the created order" do
-      @order_repository.should_receive(:save).with(@unsaved_order)
+      order_repository.should_receive(:save).with(unsaved_order)
 
-      @default_order_service.placeOrderFor(@products, @user)
+      default_order_service.placeOrderFor(products, user)
     end
 
     it "should charge for the saved order via the payment gateway" do
-      @payment_gateway.should_receive(:payFor).with(@saved_order)
+      payment_gateway.should_receive(:payFor).with(saved_order)
 
-      @default_order_service.placeOrderFor(@products, @user)
+      default_order_service.placeOrderFor(products, user)
     end
 
     it "should charge for an order whose status is new" do
-      @payment_gateway.should_receive(:payFor).with { |order| order.status == OrderStatus::NEW }
+      payment_gateway.should_receive(:payFor).with { |order| order.status == OrderStatus::NEW }
 
-      @default_order_service.placeOrderFor(@products, @user)
+      default_order_service.placeOrderFor(products, user)
     end
 
     it "should charge for the saved order after the created order is persisted" do
-      @order_repository.should_receive(:save).with(@unsaved_order) do
-        @payment_gateway.should_receive(:payFor).with(@saved_order)
-      end.and_return(@saved_order)
+      order_repository.should_receive(:save).with(unsaved_order) do
+        payment_gateway.should_receive(:payFor).with(saved_order)
+      end.and_return(saved_order)
 
-      @default_order_service.placeOrderFor(@products, @user)
+      default_order_service.placeOrderFor(products, user)
     end
 
     it "should persist the paid order after the payment is made" do
-      @payment_gateway.should_receive(:payFor).with(@saved_order) do
-        @order_repository.should_receive(:save).with(@saved_order)
+      payment_gateway.should_receive(:payFor).with(saved_order) do
+        order_repository.should_receive(:save).with(saved_order)
       end
 
-      @default_order_service.placeOrderFor(@products, @user)
+      default_order_service.placeOrderFor(products, user)
     end
 
     it "should return the last saved order" do
       second_saved_order = OrderMother.createOrder
-      @order_repository.stub(:save).with(@saved_order).and_return(second_saved_order)
+      order_repository.stub(:save).with(saved_order).and_return(second_saved_order)
 
-      @default_order_service.placeOrderFor(@products, @user).should eq(second_saved_order)
+      default_order_service.placeOrderFor(products, user).should eq(second_saved_order)
     end
 
   end
--- src/test/ruby/net/ueckerman/payment/logging_payment_gateway_spec.rb
+++ src/test/ruby/net/ueckerman/payment/logging_payment_gateway_spec.rb
@@ -7,31 +7,28 @@ import Java::org.slf4j.Logger
 
 describe LoggingPaymentGatewayDecorator do
 
-  before(:each) do
-    @delegate = double(PaymentGateway)
-    @log = double(Logger, :info => nil)
+  let(:delegate) { double(PaymentGateway) }
+  let(:log) { double(Logger, :info => nil) }
+  let(:order) { OrderMother.createOrder }
 
-    @order = OrderMother.createOrder
-
-    @decorator = LoggingPaymentGatewayDecorator.new(@delegate, @log)
-  end
+  let(:decorator) { LoggingPaymentGatewayDecorator.new(delegate, log) }
 
   context "#payFor" do
 
     it "should log the payment is initiated before the payment is processed" do
-      @log.should_receive(:info).with(/#{Regexp.escape(@order.to_s)}.*initiated/) do
-        @delegate.should_receive(:payFor).with(@order)
+      log.should_receive(:info).with(/#{Regexp.escape(order.to_s)}.*initiated/) do
+        delegate.should_receive(:payFor).with(order)
       end
 
-      @decorator.pay_for(@order)
+      decorator.pay_for(order)
     end
 
     it "should log the payment is completed after the payment is processed" do
-      @delegate.should_receive(:payFor).with(@order) do
-         @log.should_receive(:info).with(/#{Regexp.escape(@order.to_s)}.*completed/)
+      delegate.should_receive(:payFor).with(order) do
+         log.should_receive(:info).with(/#{Regexp.escape(order.to_s)}.*completed/)
       end
 
-      @decorator.pay_for(@order)
+      decorator.pay_for(order)
     end
 
   end
